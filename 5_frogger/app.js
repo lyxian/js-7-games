1c1104badb77d156a388dbe14ad197fc3f6c200c
lyx <yingxian_lai@hotmail.com>
2022-03-15 01:08:34 +0800

    comp - lvl 5

--- 5_frogger/app.js
+++ 5_frogger/app.js
@@ -1,36 +1,176 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const startButton = document.querySelector('#start-button')
+const squares = document.querySelectorAll('.grid div')
+const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
+const gridWidth = 9
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    computerChoiceDisplay.innerHTML = generateComputerChoice()
-    resultDisplay.innerHTML = getResult()
-}))
+let currentIndex = 76 //
+let currentTime = 20
+let timerId
+let resultTimerId
 
-function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
-    computerChoice = possibleChoices[randomNumber].id
-    return computerChoice
+function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
+    switch (e.key) {
+        case 'ArrowLeft':
+            if (currentIndex % gridWidth !== 0) currentIndex -= 1
+            break
+        case 'ArrowRight':
+            if (currentIndex % gridWidth < gridWidth - 1) currentIndex += 1
+            break
+        case 'ArrowUp':
+            if (currentIndex - gridWidth >= 0) currentIndex -= gridWidth
+            break
+        case 'ArrowDown':
+            if (currentIndex + gridWidth < gridWidth * gridWidth) currentIndex += gridWidth
+            break
+    }
+    squares[currentIndex].classList.add('frog')
 }
 
-function getResult() {
-    if (computerChoice == userChoice) {
-        result = 'DRAW'
-    }
-    else {
-        const key = userChoice + computerChoice
-        const possibleResults = {
-            'RockScissors': 'WIN',
-            'ScissorsPaper': 'WIN',
-            'PaperRock': 'WIN',
-            'ScissorsRock': 'LOSE',
-            'PaperScissors': 'LOSE',
-            'RockPaper': 'LOSE'
-        }
-        result = possibleResults[key]
-    }
-    return result
+squares[currentIndex].classList.add('frog')
+
+document.addEventListener('keyup', moveFrog)
+
+
+function autoMoveElements() {
+    --currentTime
+    timeLeftDisplay.textContent = currentTime
+    logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    logsRight.forEach(logRight => moveLogRight(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
 }
+
+function checkResult() {
+    win()
+    lose()
+}
+
+function moveLogLeft(logLeft) {
+    switch (true) {
+        case logLeft.classList.contains('l1'):
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
+            break
+        case logLeft.classList.contains('l2'):
+            logLeft.classList.remove('l2')
+            logLeft.classList.add('l3')
+            break
+        case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
+            break
+        case logLeft.classList.contains('l4'):
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
+            break
+        case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
+            break
+    }
+}
+
+function moveLogRight(logRight) {
+    switch (true) {
+        case logRight.classList.contains('l1'):
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+        case logRight.classList.contains('l2'):
+            logRight.classList.remove('l2')
+            logRight.classList.add('l1')
+            break
+        case logRight.classList.contains('l3'):
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break
+        case logRight.classList.contains('l4'):
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break
+        case logRight.classList.contains('l5'):
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
+            break
+    }
+}
+
+function moveCarLeft(carLeft) {
+    switch (true) {
+        case carLeft.classList.contains('c1'):
+            carLeft.classList.remove('c1')
+            carLeft.classList.add('c2')
+            break
+        case carLeft.classList.contains('c2'):
+            carLeft.classList.remove('c2')
+            carLeft.classList.add('c3')
+            break
+        case carLeft.classList.contains('c3'):
+            carLeft.classList.remove('c3')
+            carLeft.classList.add('c1')
+            break
+    }
+}
+
+function moveCarRight(carRight) {
+    switch (true) {
+        case carRight.classList.contains('c1'):
+            carRight.classList.remove('c1')
+            carRight.classList.add('c3')
+            break
+        case carRight.classList.contains('c2'):
+            carRight.classList.remove('c2')
+            carRight.classList.add('c1')
+            break
+        case carRight.classList.contains('c3'):
+            carRight.classList.remove('c3')
+            carRight.classList.add('c2')
+            break
+    }
+}
+
+function lose() {
+    if (
+        squares[currentIndex].classList.contains('c1') ||
+        squares[currentIndex].classList.contains('l4') ||
+        squares[currentIndex].classList.contains('l5') ||
+        currentTime <= 0
+    ) {
+        resultDisplay.textContent = 'You Lose'
+        clearInterval(timerId)
+        clearInterval(resultTimerId)
+        squares[currentIndex].classList.remove('frog')
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+function win() {
+    if (squares[currentIndex].classList.contains('ending-block')) {
+        resultDisplay.textContent = 'You Win'
+        clearInterval(timerId)
+        clearInterval(resultTimerId)
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+timerId = setInterval(autoMoveElements, 1000)
+resultTimerId = setInterval(checkResult, 50)
+
+startButton.addEventListener('click', () => {
+    if (timerId) {
+        clearInterval(timerId)
+        clearInterval(resultTimerId)
+        timerId = null
+        document.removeEventListener('keyup', moveFrog)
+    } else {
+        timerId = setInterval(autoMoveElements, 1000)
+        resultTimerId = setInterval(checkResult, 50)
+        document.addEventListener('keyup', moveFrog)
+    }
+})
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ https://www.freecodecamp.org/news/learn-javascript-by-coding-7-games/
 5. Frogger
 6. Connect Four
 7. Space Invaders
+
+# MISC
+
+- https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
